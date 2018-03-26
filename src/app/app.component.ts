f14ca3b890f375761c3f1e1ac0e3e8c4bf9a7226
Guest User <Guest@Epicodus-6B.local>
2018-03-26 13:37:44 -0700

    css styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { Task } from './models/task.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  tasks: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 2)
+  ];
+   editTask() {
+    alert("Time to edit a task!");
+  }
+  priorityColor(currentTask){
+  if (currentTask.priority === 3){
+    return "bg-danger";
+  } else if (currentTask.priority === 2) {
+    return  "bg-warning";
+  } else {
+    return "bg-info";
+  }
+}
 }
